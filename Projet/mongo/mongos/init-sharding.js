e75f178459d62e355eddf0b5776b1d5e4fe60130
AlexisBCD <alexisuphf@gmail.com>
2025-02-25 23:35:55 +0100

    Ajout des commandes

--- Projet/mongo/mongos/init-sharding.js
+++ Projet/mongo/mongos/init-sharding.js
@@ -26,7 +26,7 @@ db.createUser({
 db.createCollection("groupes");
 db.createCollection("membres");
 db.createCollection("materiels");
-db.createCollection("commande");
+db.createCollection("commandes");
 
 /*
 ====================================================================================================
@@ -41,8 +41,8 @@ db.membres.createIndex({ codePostal: 1 });
 db.materiels.createIndex({ codePostal: 1 });
 db.materiels.createIndex({ idGroupe: 1 });
 
-db.commande.createIndex({ codePostal: 1 });
-db.commande.createIndex({ idMembre: 1 });
+db.commandes.createIndex({ codePostal: 1 });
+db.commandes.createIndex({ idMembre: 1 });
 
 /*
 ====================================================================================================
@@ -58,7 +58,7 @@ sh.enableSharding("mydatabase");
 sh.shardCollection("mydatabase.groupes", { codePostal: 1 });
 sh.shardCollection("mydatabase.membres", { codePostal: 1 });
 sh.shardCollection("mydatabase.materiels", { codePostal: 1 });
-sh.shardCollection("mydatabase.commande", { codePostal: 1 });
+sh.shardCollection("mydatabase.commandes", { codePostal: 1 });
 
 // Configuration des plages de données pour les shards
 sh.splitAt("mydatabase.groupes", { codePostal: 50000 });
@@ -73,9 +73,9 @@ sh.splitAt("mydatabase.materiels", { codePostal: 50000 });
 sh.moveChunk("mydatabase.materiels", { codePostal: 0 }, "shard0");
 sh.moveChunk("mydatabase.materiels", { codePostal: 50001 }, "shard1");
 
-sh.splitAt("mydatabase.commande", { codePostal: 50000 });
-sh.moveChunk("mydatabase.commande", { codePostal: 0 }, "shard0");
-sh.moveChunk("mydatabase.commande", { codePostal: 50001 }, "shard1");
+sh.splitAt("mydatabase.commandes", { codePostal: 50000 });
+sh.moveChunk("mydatabase.commandes", { codePostal: 0 }, "shard0");
+sh.moveChunk("mydatabase.commandes", { codePostal: 50001 }, "shard1");
 
 
 /*
@@ -100,6 +100,7 @@ db.membres.insertMany([
     { _id: ObjectId('67800070c5f2fb32c3f37bc2'), nom: "Durand", prenom: "Marie", adresse: "20 Rue de Lyon", ville: "Lyon", codePostal: 69000, email: "marie.durand@example.com", typeMembre: "CLIENT", password: "securePass456", confirmPassword: "securePass456", _class: "org.example.classes.Membre" },
     { _id: ObjectId('67800070c5f2fb32c3f37bc3'), nom: "Martin", prenom: "Paul", adresse: "30 Rue de Nice", ville: "Nice", codePostal: 6000, email: "paul.martin@example.com", typeMembre: "CLIENT", password: "niceDay789", confirmPassword: "niceDay789", _class: "org.example.classes.Membre" },
     { _id: ObjectId('67800070c5f2fb32c3f37bc4'), nom: "Bernard", prenom: "Claire", adresse: "40 Rue de Caen", ville: "Caen", codePostal: 14000, email: "claire.bernard@example.com", typeMembre: "CLIENT", password: "clairePass101", confirmPassword: "clairePass101", _class: "org.example.classes.Membre" },
+    { _id: ObjectId('67be408f933d5116a45a1550'), nom: "a", prenom: "a", adresse: "a", ville: "a", codePostal: 59135, email: "i@yop", typeMembre: "ACTIF", _class: "org.example.classes.Membre" },
     { _id: ObjectId('67800070c5f2fb32c3f37bc5'), nom: "Legrand", prenom: "Alice", adresse: "50 Rue de Lille", ville: "Lille", codePostal: 59000, email: "alice.legrand@example.com", typeMembre: "CLIENT", password: "aliceKey202", confirmPassword: "aliceKey202", _class: "org.example.classes.Membre" }
 ]);
 
@@ -112,11 +113,11 @@ db.materiels.insertMany([
     { _id: ObjectId(), numeroSerie: "567890", modele: "VivoBook", type: "LAPTOP", marque: "Asus", prix: 700, _class: "org.example.classes.Materiel" }
 ]);
 
-// Insertion des données dans "commande"
-db.commande.insertMany([
-    { idCommande: 1, idMembre: 1, date: new Date("2024-12-01"), liste: [{ idMateriel: 1, quantite: 1 }], prixTotal: 500.0, codePostal: 75001 },
-    { idCommande: 2, idMembre: 2, date: new Date("2024-12-05"), liste: [{ idMateriel: 2, quantite: 2 }], prixTotal: 1200.0, codePostal: 69000 },
-    { idCommande: 3, idMembre: 3, date: new Date("2024-12-10"), liste: [{ idMateriel: 3, quantite: 1 }], prixTotal: 550.0, codePostal: 6000 },
-    { idCommande: 4, idMembre: 4, date: new Date("2024-12-15"), liste: [{ idMateriel: 4, quantite: 1 }], prixTotal: 1200.0, codePostal: 14000 },
-    { idCommande: 5, idMembre: 5, date: new Date("2024-12-20"), liste: [{ idMateriel: 5, quantite: 1 }], prixTotal: 700.0, codePostal: 59000 }
+// Insertion des données dans "commandes"
+db.commandes.insertMany([
+    { idMembre: { "$ref": "membres", "$id": { "$oid": "67800070c5f2fb32c3f37bc3" } }, idVendeur: { "$ref": "membres", "$id": { "$oid": "67be408f933d5116a45a1550" } }, date: new Date("2024-11-15T10:30:00Z"), liste: [{ idMateriel: 1, numeroSerie: "123456", modele: "Inspiron", type: "LAPTOP", marque: "Dell", prix: 500 }], prixTotal: 500, codePostal: 75001 },
+    { idMembre: { "$ref": "membres", "$id": { "$oid": "67800070c5f2fb32c3f37bc3" } }, idVendeur: { "$ref": "membres", "$id": { "$oid": "67be408f933d5116a45a1550" } }, date: new Date("2025-01-05T14:00:00Z"), liste: [{ idMateriel: 2, numeroSerie: "789012", modele: "Pavilion", type: "LAPTOP", marque: "HP", prix: 600 }, { idMateriel: 3, numeroSerie: "345678", modele: "ThinkPad", type: "LAPTOP", marque: "Lenovo", prix: 550 }], prixTotal: 1150, codePostal: 69000 },
+    { idMembre: { "$ref": "membres", "$id": { "$oid": "67800070c5f2fb32c3f37bc3" } }, idVendeur: { "$ref": "membres", "$id": { "$oid": "67be408f933d5116a45a1550" } }, date: new Date("2025-02-10T09:15:00Z"), liste: [{ idMateriel: 4, numeroSerie: "901234", modele: "MacBook", type: "LAPTOP", marque: "Apple", prix: 1200 }], prixTotal: 1200, codePostal: 6000 },
+    { idMembre: { "$ref": "membres", "$id": { "$oid": "67800070c5f2fb32c3f37bc3" } }, idVendeur: { "$ref": "membres", "$id": { "$oid": "67be408f933d5116a45a1550" } }, date: new Date("2025-03-22T18:45:00Z"), liste: [{ idMateriel: 5, numeroSerie: "567890", modele: "VivoBook", type: "LAPTOP", marque: "Asus", prix: 700 }, { idMateriel: 6, numeroSerie: "yu", modele: "yu", type: "ECRAN", marque: "yu", prix: 5 }], prixTotal: 705, codePostal: 14000 },
+    { idMembre: { "$ref": "membres", "$id": { "$oid": "67800070c5f2fb32c3f37bc3" } }, idVendeur: { "$ref": "membres", "$id": { "$oid": "67be408f933d5116a45a1550" } }, date: new Date("2025-04-10T20:10:00Z"), liste: [{ idMateriel: 1, numeroSerie: "123456", modele: "Inspiron", type: "LAPTOP", marque: "Dell", prix: 500 }, { idMateriel: 3, numeroSerie: "345678", modele: "ThinkPad", type: "LAPTOP", marque: "Lenovo", prix: 550 }, { idMateriel: 5, numeroSerie: "567890", modele: "VivoBook", type: "LAPTOP", marque: "Asus", prix: 700 }], prixTotal: 1750, codePostal: 59000 }
 ]);
